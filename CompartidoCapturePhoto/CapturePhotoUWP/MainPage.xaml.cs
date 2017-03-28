94a70ad0cd8b3fe0061e51f037d8996a1c19852d
Dante Santelli <dante.a.santelli@gmail.com>
2017-03-28 12:42:56 -0300

    Capturando Foto en Windows OK

---
+++ CompartidoCapturePhoto/CapturePhotoUWP/MainPage.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Media.Capture;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.Storage.Streams;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace CapturePhotoUWP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+  
+    
+
+    public sealed partial class MainPage : Page
+    {
+        private StorageFile storeFile;
+        private IRandomAccessStream stream;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            
+        }
+
+        private async void captureBtn_Click(object sender, RoutedEventArgs e)
+        {
+            CameraCaptureUI capture = new CameraCaptureUI();
+            capture.PhotoSettings.Format = CameraCaptureUIPhotoFormat.Jpeg;
+            capture.PhotoSettings.CroppedAspectRatio = new Size(3, 5);
+            capture.PhotoSettings.MaxResolution = CameraCaptureUIMaxPhotoResolution.HighestAvailable;
+            storeFile = await capture.CaptureFileAsync(CameraCaptureUIMode.Photo);
+            if (storeFile != null)
+            {
+                BitmapImage bimage = new BitmapImage();
+                stream = await storeFile.OpenAsync(FileAccessMode.Read); ;
+                bimage.SetSource(stream);
+                captureImage.Source = bimage;
+
+            }
+        }
+
+        //Next goto savebutton clickevent and writethe followingcode tosave thecaptured image.
+        private async void saveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+
+                FileSavePicker fs = new FileSavePicker();
+                fs.FileTypeChoices.Add("Image", new List<string>() { ".jpeg" });
+                fs.DefaultFileExtension = ".jpeg";
+                fs.SuggestedFileName = "Image" + DateTime.Today.ToString();
+                fs.SuggestedStartLocation = PickerLocationId.PicturesLibrary;
+                fs.SuggestedSaveFile = storeFile;
+                // Saving the file 
+                var s = await fs.PickSaveFileAsync();
+                if (s != null)
+                {
+                    using (var dataReader = new DataReader(stream.GetInputStreamAt(0)))
+                    {
+                        await dataReader.LoadAsync((uint)stream.Size);
+                        byte[] buffer = new byte[(int)stream.Size];
+                        dataReader.ReadBytes(buffer);
+                        await FileIO.WriteBytesAsync(s, buffer);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                var messageDialog = new MessageDialog("Unable to save now. " + ex.Message);
+                await messageDialog.ShowAsync();
+            }
+        }
+    }
+
+    
+
+    
+
+}
+
+
