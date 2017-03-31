751f13d6687b92f7463e8069eba479514952da34
Dante Santelli <dante.a.santelli@gmail.com>
2017-03-30 22:34:56 -0300

    Listo

--- CompartidoCapturePhoto/CapturePhotoAndroid/MainActivity.cs
+++ CompartidoCapturePhoto/CapturePhotoAndroid/MainActivity.cs
@@ -12,8 +12,53 @@ namespace CapturePhotoAndroid
             base.OnCreate(bundle);
 
             // Set our view from the "main" layout resource
-            // SetContentView (Resource.Layout.Main);
+            SetContentView(Resource.Layout.Main);
+
+            //Cambiar si es un edit
+            this.Title = "Agregar trago";
+
+            /*
+            Spinner spinnerCategoria = FindViewById<Spinner>()
+
+            ArrayAdapter<string> adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleSpinnerDropDownItem) {
+
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent)
+        {
+
+            View v = super.getView(position, convertView, parent);
+            if (position == getCount())
+            {
+                ((TextView)v.findViewById(android.R.id.text1)).setText("");
+                ((TextView)v.findViewById(android.R.id.text1)).setHint(getItem(getCount())); //"Hint to be displayed"
+            }
+
+            return v;
         }
+
+        
+            public int getCount()
+        {
+            return super.getCount() - 1; // you dont display last item. It is used as hint.
+        }
+
+    };
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            adapter.add("Daily");
+            adapter.add("Two Days");
+            adapter.add("Weekly");
+            adapter.add("Monthly");
+            adapter.add("Three Months");
+            adapter.add("HINT_TEXT_HERE"); //This is the text that will be displayed as hint.
+
+
+            spinner.setAdapter(adapter);
+            spinner.setSelection(adapter.getCount()); //set the hint the default selection so it appears on launch.
+            spinner.setOnItemSelectedListener(this);
+        */
+        }
+
     }
 }
 
--- CompartidoCapturePhoto/CapturePhotoUWP/MainPage.xaml.cs
+++ CompartidoCapturePhoto/CapturePhotoUWP/MainPage.xaml.cs
@@ -5,10 +5,12 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
-using Windows.Media.Capture;
-using Windows.Storage;
+using Windows.Graphics.Imaging;
+using Windows.Media.Capture;            //To use the camera capture UI
+using Windows.Storage;                  //To do file operations with the returned image file
 using Windows.Storage.Pickers;
 using Windows.Storage.Streams;
+//using Windows.Graphics.Imaging;
 using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -20,6 +22,15 @@ using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // Package.appxmanifest -> View Designer -> Capabilities -> WebCam y Pictures Library
+/*
+ * First we need to enable the following capability’s “Pictures Library” and “Webcam” 
+ * in the Package.appxmanifest file like the following image.
+ * Next we need to add a one Button to capture Image and an Image control to show
+ *  the captured image. Then we will save the image in the location of the current system.
+ * Now go to MainPage.xaml and design your UI according to your wish here 
+ * I create application bar with two buttons one is for capture image and one 
+ * is for save the image. Create one image control to preview the captured image.
+ */
 
 namespace CapturePhotoUWP
 {
@@ -32,33 +43,97 @@ namespace CapturePhotoUWP
         public MainPage()
         {
             this.InitializeComponent();
-            
+
+            //Si no es un edit
+            hubTitle.Header = "Agregar trago";
+
+            //Si no existe imagen para ese trago, cargar imagen por defecto
+            BitmapImage drinkImageDefault = new BitmapImage(new Uri(this.BaseUri, "/Assets/drink-default.jpg"));
+            drinkImageCapture.Source = drinkImageDefault;
         }
 
         //Abre la cam para tomar foto y la muestra en un Image
         private async void captureBtn_Click(object sender, RoutedEventArgs e)
         {
+            //CameraCaptureUI class to capture photos or videos using the camera UI built into Windows. 
             CameraCaptureUI capture = new CameraCaptureUI();
+            /* To capture a photo, create a new CameraCaptureUI object. 
+             * Using the object's PhotoSettings property you can specify properties 
+             * for the returned photo such as the image format of the photo. 
+             * By default, the camera capture UI allows the user to crop (recortar)
+             *  the photo before it is returned, although this can be disabled with the AllowCropping
+             *  property. 
+             * This example sets the CroppedSizeInPixels to request that the returned image be 200 x 200 in pixels.
+            */
             capture.PhotoSettings.Format = CameraCaptureUIPhotoFormat.Jpeg;
             capture.PhotoSettings.CroppedAspectRatio = new Size(3, 5);
+            //capture.PhotoSettings.CroppedSizeInPixels = new Size(200, 200);
             capture.PhotoSettings.MaxResolution = CameraCaptureUIMaxPhotoResolution.HighestAvailable;
+
+            /*
+             Call CaptureFileAsync and specify CameraCaptureUIMode.Photo to specify that a photo should be 
+             captured. The method returns a StorageFile instance containing the image if the capture 
+             is successful. If the user cancels the capture, the returned object is null.
+             */
             storeFile = await capture.CaptureFileAsync(CameraCaptureUIMode.Photo);
+
             if (storeFile != null)
             {
                 BitmapImage bimage = new BitmapImage();
                 stream = await storeFile.OpenAsync(FileAccessMode.Read); ;
                 bimage.SetSource(stream);
-                captureImage.Source = bimage;
+                drinkImageCapture.Source = bimage;
 
-            }
+                //VER SI HACERLO ASI O COMO ESTA ARRIBA QUE ES MAS SENCILLO CREO
+                /*
+                 Call OpenAsync to get a stream from the image file. Call BitmapDecoder.CreateAsync 
+                 to get a bitmap decoder for the stream. 
+                 Then call GetSoftwareBitmap to get a SoftwareBitmap representation of the image.
+                 The Image control requires that the image source be in BGRA8 format with
+                 premultiplied alpha or no alpha, so call the static method SoftwareBitmap.Convert
+                 to create a new software bitmap with the desired format. 
+                 Next, create a new SoftwareBitmapSource object and call SetBitmapAsync 
+                 to assign the software bitmap to the source. Finally, set the Image control's 
+                 Source property to display the captured photo in the UI.
+                 */
+                /*
+               IRandomAccessStream stream = await storeFile.OpenAsync(FileAccessMode.Read);
+               BitmapDecoder decoder = await BitmapDecoder.CreateAsync(stream);
+               SoftwareBitmap softwareBitmap = await decoder.GetSoftwareBitmapAsync();
+               SoftwareBitmap softwareBitmapBGR8 = SoftwareBitmap.Convert(softwareBitmap,
+               BitmapPixelFormat.Bgra8, 
+               BitmapAlphaMode.Premultiplied);
+
+                SoftwareBitmapSource bitmapSource = new SoftwareBitmapSource();
+                await bitmapSource.SetBitmapAsync(softwareBitmapBGR8);
+
+                imageControl.Source = bitmapSource;
+               */
+
+                //VER SI HACE FALTA GUARDAR LA FOTO EN DISCO 
+                /*
+                El StorageFile que contiene la foto capturada recibe un nombre generado dinámicamente y 
+                se guarda en la carpeta local de la aplicación. Para organizar mejor las fotos capturadas,
+                puede que desee mover el archivo a otra carpeta.
+                */
+                //Guarda la foto en C:\Users\Dante\AppData\Local\Packages\884cbc0c-e00e-4cf6-994c-9e419a580f86_tn3w70na242fe\LocalState\DrinkPhotoFolder
+                StorageFolder destinationFolder =
+            await ApplicationData.Current.LocalFolder.CreateFolderAsync("DrinkPhotoFolder",
+                CreationCollisionOption.OpenIfExists);
+
+            await storeFile.CopyAsync(destinationFolder, "drinkPhoto.jpg", NameCollisionOption.ReplaceExisting);
+            await storeFile.DeleteAsync();
+           }
+   
+
         }
 
         //Next goto savebutton clickevent and writethe followingcode to save the captured image.
         private async void saveBtn_Click(object sender, RoutedEventArgs e)
         {
+            
             try
             {
-
                 FileSavePicker fs = new FileSavePicker();
                 fs.FileTypeChoices.Add("Image", new List<string>() { ".jpeg" });
                
@@ -84,12 +159,20 @@ namespace CapturePhotoUWP
                 var messageDialog = new MessageDialog("Unable to save now. " + ex.Message);
                 await messageDialog.ShowAsync();
             }
+        
+
+
+            
+
+        }
+
+        private void cancelBtn_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
+       
 
-    
-
-    
 
 }
 
